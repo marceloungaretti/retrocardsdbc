5269dc54bb02af6aab2ac2e8b09a93c079ebfdf6
Marcelo <marcelo.ungaretti@gmail.com>
2021-12-09 14:25:29 -0300

    projeto iniciado

---
+++ src/main/java/com/dbc/retrocards/RetrocardsApplication.java
@@ -0,0 +1,13 @@
+package com.dbc.retrocards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrocardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetrocardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbc/retrocards/RetrocardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbc.retrocards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrocardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
