7f1cf4100f64a36d7d4c733ec85ad3b084d2cbe0
Adriele Beatriz Santos <adriele.beatriz98@outlook.com>
2021-12-14 11:22:23 -0300

    alteraçoe Email service

--- src/main/java/com/dbc/retrocards/service/EmailService.java
+++ src/main/java/com/dbc/retrocards/service/EmailService.java
@@ -10,21 +10,61 @@ import com.dbc.retrocards.entity.StatusRetrospectivaEntity;
 import com.dbc.retrocards.exceptions.RegraDeNegocioException;
 import com.dbc.retrocards.repository.EmailRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import lombok.RequiredArgsConstructor;
+import freemarker.template.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
 @Service
 @RequiredArgsConstructor
 public class EmailService {
     private final ObjectMapper objectMapper;
     private final EmailRepository emailRepository;
+    private final JavaMailSender emailSender;
+    @Value("${spring.mail.username}")
+    private String remetente;
+    private final Configuration configuration;
 
-    public EmailDTO create(EmailCreateDTO emailCreateDTO) throws RegraDeNegocioException {
+
+    public EmailDTO create(EmailCreateDTO emailCreateDTO) throws RegraDeNegocioException, MessagingException, TemplateException, IOException {
         EmailEntity entity = objectMapper.convertValue(emailCreateDTO, EmailEntity.class);
-        String string = emailCreateDTO.getEmailDestinatario() + ", ";
-        entity.setEmailDestinatario(string);
         EmailEntity emailCriar = emailRepository.save(entity);
         EmailDTO emailDTO = objectMapper.convertValue(emailCriar, EmailDTO.class);
+         enviarEmailComTemplate(emailDTO);
         return emailDTO;
+    }
+
+    public void enviarEmailComTemplate( EmailDTO emailDTO) throws MessagingException, IOException, TemplateException {
+        MimeMessage mimeMessage = emailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+
+        helper.setFrom(remetente);
+        helper.setTo( emailDTO.getEmailDestinatario().split(", "));
+        helper.setSubject(emailDTO.getAssunto());
+
+        Template template = configuration.getTemplate("email-template.ftl");
+        Map<String, Object> dados = new HashMap<>();
+        dados.put("email", remetente );
+        String html = FreeMarkerTemplateUtils.processTemplateIntoString(template, dados);
+
+        helper.setText(html, true);
+
+        emailSender.send(mimeMessage);
     }
 }
