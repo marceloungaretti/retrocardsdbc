65f93a519739f0e0492c38c84169307e93a0b96e
Adriele Beatriz Santos <adriele.beatriz98@outlook.com>
2021-12-15 14:39:54 -0300

    ajustes DTOS

--- src/main/java/com/dbc/retrocards/dto/EmailCreateDTO.java
+++ src/main/java/com/dbc/retrocards/dto/EmailCreateDTO.java
@@ -1,17 +1,29 @@
 package com.dbc.retrocards.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.persistence.Column;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Data
 public class EmailCreateDTO {
 
+
+    @NotBlank
+    @NotEmpty
+    @ApiModelProperty(value = "Campo assunto do email")
     private String assunto;
 
+    @NotNull
+    @ApiModelProperty(value = "emaisl do destinatario")
     private String emailDestinatario;
 
+    @NotNull
+    @ApiModelProperty(value = "data de envio do email")
     private LocalDate dataEnvio;
 }
 
